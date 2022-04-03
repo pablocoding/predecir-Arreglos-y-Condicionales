226e6435af0e938ec9aa067483a16136e780d8cc
Juan Pablo Ibarbo <juanibarbo456@gmail.com>
2022-04-02 20:48:08 -0500

    version1

---
+++ index.js
@@ -0,0 +1,37 @@
+// Fragmento de código 1 //
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo); // resultado: ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+}
+
+// arrays            ||       valores                                                     //
+//  auntContactInfo  || ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+
+
+
+
+
+// Fragmento de código 2 //
+function displayGrocerylist() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croquetas de papa");
+    console.log(frozen); // resultados:["brócoli", "helado", "croquetas de papa"]
+}
+
+// arrays  || valores                    //
+// produce || ["manzanas", "naranjas"]
+// frozen  || ["brócoli", "helado"]
+
+
+
+
+// Fragmento de código 3 //
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro"); // agregarlo
+movieLibrary[1] = "Beetlejuice"; // actualizarlo
+console.log(movieLibrary); // resultado: ['Bambi', 'Beetlejuice', 'Toy Story', 'Zoro']
+
+//  arrays          ||      valores                    //
+//  movieLibrary    || ["Bambi", "E.T.", "Toy Story"]
+
